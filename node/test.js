f1f1e6295fac3c577674cf1005dc33a52d011e06
Trafalgar99 <1286015267@qq.com>
2020-10-20 15:51:02 +0800

    2020年10月20日 15:51:01

--- node/test.js
+++ node/test.js
@@ -1,3 +1,12 @@
-const url = require("url");
+const readline = require("readline");
 
-console.log(url.parse("http://www.baodu.com/login.html"));
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.question("你如何看待 Node.js 中文网？", (answer) => {
+  // TODO：将答案记录在数据库中。
+  console.log(`感谢您的宝贵意见：${answer}`);
+
+  rl.close();
+});
